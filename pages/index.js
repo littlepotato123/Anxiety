cf87910e9f028c781ae970656bf184ef331a932b
littlepotato123 <sopanda0910@gmail.com>
2021-11-01 20:59:58 -0400

    All Information

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,126 @@ import Link from 'next/link'
 export default function Home() {
   return ( 
     <div>
-      <Link href="/coping"><h1>Coping Link</h1></Link>
+      <h1>Types of Anxiety</h1>
+      <ul>
+        <li>Panic Disorder: Experiencing recurring panic attacks at unexpected times. A person with panic disorder may live in fear of the next panic attack</li> 
+        <li>Phobia: Excessive fear of a specific object, situation, or activity</li>
+        <li>Social Anxiety Disorder: Extreme fear of being judged by others in social situations</li>
+        <li>Obsessive-Compulsive Disorder: Recurring irrational thoughts that lead you to perform specific, repeated behaviors</li>
+        <li>Seperation Anxiety Disorder: Fear of being away from home or loved ones</li>
+        <li>Illness Anxiety Disorder: Anxiety about your health (formerly called hypochondria)</li>
+        <li>Post-traumatic Stress Disorder (PTSD): Anxiety following a traumatic event</li>
+      </ul>
+      <h1>Symptoms of Anxiety</h1>
+      <ul>
+        <li>Increased Heart Rate</li>
+        <li>Rapid Breathing</li>
+        <li>Restlessness</li>
+        <li>Trouble Concentrating</li>
+        <li>Difficulty Falling Asleep</li>
+      </ul>
+      <h1>What is an Anxiety Attack</h1>
+      <p></p>
+      <ul>
+        <li>Feeling Faint or Dizzy</li>
+        <li>Shortness of Breath</li>
+        <li>Dry Mouth</li>
+        <li>Sweating</li>
+        <li>Chills or Hot Flashes</li>
+        <li>Apprehension and worry</li>
+        <li>Restlessness</li>
+        <li>Distress</li>
+        <li>Fear</li>
+        <li>Numbness or Tingling</li>
+      </ul>
+      <h1>Causes of Anxiety</h1>
+      <p>Based on current research, scientists are completely aware of the exact cause of anxiety, but the assume that it is likely a combination of factors. These may include genetic, environmental and neuro-chemical factors that play a role in enhancing one's stress levels and fear factor. Researches also believe that the areas of the brain responsible for controlling fear may be impacted or damaged.</p>
+      <h1>Tests to Diagnose Anxiety</h1>
+      <p>There is not a single to determine whether a patient has anxiety, but instead there is a lengthy process of physical examinations, mental evaluations, and psychological questionnaires. Some doctors conduct physical exams such as blood and uring tests to rule out other conditions. Some anxiety tests are not to determine if anxiety exists, but instead the severity of the diagnosis.</p>
+      <h1>Treatments for Anxiety</h1>
+      <p>There are two types of "treatments" and they are based on the severity of the anxiety, the patient's preference, and also based on what the medical practitioners recommends.</p>
+      <ul>
+        <li>
+          <h1>Psychotherapy</h1>
+          <p>Meeting with a therapist can help patients learn tools to use and strategies to cope with anxiety when it occurs. This can also help with having an outlet, preventing some symptoms, and understanding how to live with some symptoms.</p>
+        </li>
+        <li>
+          <h1>Medication</h1>
+          <p>
+            Medications typically used to treat anxiety include antidepressants and sedatives, and they work to balance brain chemistry, prevent episodes of anxiety, and ward off the most severe symptoms of the disorder.
+          </p>
+        </li>
+        <li>
+          <Link href="/coping">Coping Strategies</Link>
+        </li>
+      </ul>
+      <h2>Natural Remedies</h2>
+      <ul>
+        <li>Yoga</li>
+        <li>Getting Enough Sleep</li>
+        <li>Meditating</li>
+        <li>Staying Active Excercising</li>
+        <li>Eating a Healthy Diet</li>
+        <li>Avoiding Alcohol</li>
+        <li>Avoiding Caffeine</li>
+        <li>Quitting Smoking Cigarettes</li>
+        <li>
+          <Link href="/coping">Coping Strategies</Link>
+        </li>
+      </ul>
+      <h1>Therapies for Anxiety</h1>
+      <ul>
+        <li>
+          <h1>Talk Therapy</h1>
+          <p>
+            Also known as psychotherapy, where stress is remedied by communication to identify and solve issues that cause emotional distress. This helps because it serves as an outlet for people that prefer to verbalize their stress.
+          </p>
+        </li>
+        <li>
+          <h1>Art Therapy</h1>
+          <p>
+            Art therapy involves allowing kids to make art for their own relaxation and sometimes for therapists to interpret. This works because children who are unable to communicat etheir feelings and built-up stress can express it through art.
+          </p>
+        </li>
+        <li>
+          <h1>Deep Pressure Therapy</h1>
+          <p>
+            Deep Pressure Therapy consists of applying gentle but firm pressure to the body with a pressure garment. This helps because it is similar to hugging and creating a comfortable environment where stress melts away.
+          </p>
+        </li>
+      </ul>
+      <h1>Anxiety and Stress</h1>
+      <p>
+        They both are two sides of the same coin. Stress is the result of demands on your brain or body. It can be caused by an event or activity that makes one nervous. Anxiety is that same worry or uneasiness.
+      </p>
+      <p>Both their symptoms include: </p>
+      <ul>
+        <li>Headache</li> 
+        <li>Stomache</li>
+        <li>Fast Heartbeat</li>
+        <li>Sweating</li>
+        <li>Dizziness</li>
+        <li>Jitteriness</li>
+        <li>Muscle Tension</li>
+        <li>Rapid Breathing</li>
+        <li>Panic</li>
+        <li>Nervousness</li>
+        <li>Difficulty Concentrating</li>
+        <li>Irrational Anger or Irritability</li>
+        <li>Restlessness</li>
+        <li>Sleeplessness</li>
+      </ul>
+      <p>Anxiety and Stress are both a negative and positive and can act as an incentive for some. But with the same issue, it can be the downfall to others.</p>
+      <h1>Sources</h1>
+      <ul>
+        <p>
+          https://www.healthline.com/health/anxiety/anxiety-symptoms-in-children
+        </p>
+        <p>
+          https://www.healthline.com/health/anxiety
+        </p>
+        <p>https://www.healthline.com/health/mental-health/talk-therapy</p>
+      </ul>
     </div>
   )
 }
